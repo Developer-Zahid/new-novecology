e04b0503f2acfc803b8696f008f0d012f73f6e6b
Developer-Zahid <coderexpertzahid@gmail.com>
2023-04-20 22:12:23 +0600

    new novecology website design init

---
+++ assets/js/script.js
@@ -0,0 +1,79 @@
+(function ($) {
+    "use strict"
+
+	/* Document on load functions */
+	$(window).on('load', function () {
+        // preLoader();
+		headerHeightFixer();
+    });
+
+	/* Preloader init */
+	function preLoader(){
+		$(".preloader").delay(1000).fadeOut("slow");
+	}
+
+	/* scroll top btn */
+	$(".scroll-top").on("click", function () {
+		$("html,body").animate({scrollTop: 0},50);
+	});
+	$(window).on("scroll", function () {
+		var scrolling = $(this).scrollTop();
+
+		if (scrolling > 200) {
+			$(".scroll-top").fadeIn();
+		} else {
+			$(".scroll-top").fadeOut();
+		}
+	});
+
+	/* Fix Header Height function */
+	$('header').before('<div class="header-height-fix"></div>');
+    function headerHeightFixer(){
+    	$('.header-height-fix').css('height', $('header').innerHeight() +'px');
+	};
+
+    /*  Banner slider */
+    $(".banner__slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+		fade: true,
+		cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 8000,
+        speed: 600,
+        arrows: true,
+        prevArrow: '<button class="slick__arrows slick__arrows--left border-0 d-inline-flex align-items-center justify-content-center position-absolute"><i class="bi bi-chevron-left"></i></button>',
+		nextArrow: '<button class="slick__arrows slick__arrows--right border-0 d-inline-flex align-items-center justify-content-center position-absolute"><i class="bi bi-chevron-right"></i></button>',
+        dots: false,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        infinite: true
+    });
+
+
+    /*  common slider */
+    // $(".common__slider").slick({
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     autoplay: true,
+    //     autoplaySpeed: 4000,
+    //     speed: 500,
+    //     arrows: true,
+    //     prevArrow: '<button class="slick__arrows slick__arrows--left border-0 d-inline-flex align-items-center justify-content-center position-absolute"><i class="bi bi-chevron-left"></i></button>',
+	// 	nextArrow: '<button class="slick__arrows slick__arrows--right border-0 d-inline-flex align-items-center justify-content-center position-absolute"><i class="bi bi-chevron-right"></i></button>',
+    //     dots: false,
+    //     pauseOnHover: false,
+    //     pauseOnFocus: false,
+    //     infinite: true,
+	// 	responsive: [
+	// 		{
+	// 			breakpoint: 768,
+	// 			settings: {
+	// 				arrows: false,
+	// 				dots: true
+	// 			}
+	// 		},
+	// 	]
+    // });
+
+})(jQuery);
